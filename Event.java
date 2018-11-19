f3fd214881cc99bd700fdc66a8fe2046248739fa
elixir88 <ognjen.andric@gmail.com>
2018-11-19 12:50:27 +0100

    Update Event.java
    sorry

--- Event.java
+++ Event.java
@@ -1,41 +1,30 @@
-public class Event{
+public class Event {
+  //Attributes
+  private char type;
+  private double time;
+  private Individual individual;
 
-	private Individual;
-	private double time;
-	private char type;
-	private double proba;
+  //Constructor
+  public Event(char type, double time, Individual individual){
+    this.type = type;
+    this.time = time;
+    this.individual = individual;
+  }
 
-	public Event(Individual individual, double time, char type){
-		this.Individual = individual;
-		this.time = time;
-		this.type = type;
+  //Getters
+  public Individual individual() {
+    return individual;
+  }
+  public double time() {
+    return time;
+  }
+  public char type() {
+    return type;
+  }
 
-	}
+  //Returns a textual representation of this event.
+  public String toString() {
+    return "Event type: " + type() + ", time: " + time() + ", individual: " + individual();
+  }
 
-	public
-
-
-	public double time(){
-		return time;
-	}
-
-	public char type(){
-		return type;
-	}
-
-	public Individual individual(){
-		return individual;
-	}
-
-	public String toString(){
-		if(type = 'M'){
-			return "Mutation"
-		}
-		if(type = 'R'){
-			return "Reproduction"
-		}
-		else{
-			return "Death"
-		}
-	}
 }
