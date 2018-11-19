51ce4a3d14f85460e0a0dc3231ea54a1bf204fd9
elixir88 <ognjen.andric@gmail.com>
2018-11-19 14:24:26 +0100

    Update Individual.java
    tilføjet linie 12

--- Individual.java
+++ Individual.java
@@ -1,15 +1,21 @@
 public class Individual {
   //Attributes
-
+  private City[] cities;
 
   //Constructor
   public Individual(City[] cities) {
+    int random = RandomUtils.getRandomValue(cities.length);
+    int lastIndex = cities.length - 1;
+    City aux = cities[random];
+    cities[random] = cities[lastIndex];
+    cities[lastIndex] = aux;
+    this.cities = cities; 
     
   }
 
   //Returns a copy of this individual's path.
   public City[] path() {
-    
+
   }
 
   //Returns the cost assigned to this individual's path.
@@ -19,12 +25,13 @@ public class Individual {
 
   //Performs a mutation of this individual.
   public void mutate() {
-    
+
   }
 
   //Returns a new Individual differing from this one by a mutiation.
   public Individual reproduce() {
-    
+
   }
 }
+
 
