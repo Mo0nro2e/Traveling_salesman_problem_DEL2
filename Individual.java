ec54af7493ce80e7c2f21037b87f75965373912e
elixir88 <ognjen.andric@gmail.com>
2018-11-19 12:55:09 +0100

    Update Individual.java
    skabelon lalala

--- Individual.java
+++ Individual.java
@@ -1,3 +1,30 @@
-public class Individual{
+public class Individual {
+  //Attributes
 
+
+  //Constructor
+  public Individual(City[] cities) {
+    
+  }
+
+  //Returns a copy of this individual's path.
+  public City[] path() {
+    
+  }
+
+  //Returns the cost assigned to this individual's path.
+  public double cost() {
+    
+  }
+
+  //Performs a mutation of this individual.
+  public void mutate() {
+    
+  }
+
+  //Returns a new Individual differing from this one by a mutiation.
+  public Individual reproduce() {
+    
+  }
 }
+
