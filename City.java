fd031840e589f8710359ad2f73a33a4399322388
Tobias B. Rasch <tobiaz91@gmail.com>
2018-11-19 11:44:21 +0100

    update

--- City.java
+++ City.java
@@ -1,15 +1,11 @@
 public class City{
 	private String name;
-	private double xCoordinate;
-	private double yCoordinate;
+	private double yCoordinate, xCoordinate ;
 
 	public City(String name, double xCoordinate, double yCoordinate){
 		this.name = name;
-		System.out.println("The name of your city is" + name)
 		this.xCoordinate = xCoordinate;
-		System.out.println("" + name)
 		this.yCoordinate = yCoordinate;
-		System.out.println("with the coordinates" + yCoordinate "and" + xCoordinate );
 	}
 
 
@@ -26,7 +22,7 @@ public class City{
 	}
 
 	public double distanceTo(City ohter){
-		return (Math.sqat(Math.pow(this.xCoordinate - City.getX(),2) + Math.pow(this.yCoordinate - City.getY(),2)));
+		return (Math.sqat(Math.pow(this.xCoordinate - ohter.getX(),2) + Math.pow(this.yCoordinate - ohter.getY(),2)));
 	}
 
 
--- Individual.java
+++ Individual.java
@@ -1,3 +1,27 @@
 public class Individual{
 
+  private double cost;
+  private City[] path;
+
+
+    public Individual(City[] cities){
+      
+      for(int i=cities.length - 1; counter >= 0;counter--){
+        cities[i] =  cities[RandomUtils.getRandomValue(cities.length)];
+
+    }
+
+
+    public cost(){
+      return cost;
+    }
+
+    public path(){
+      return path
+    }
+
+
+
+
+
 }
