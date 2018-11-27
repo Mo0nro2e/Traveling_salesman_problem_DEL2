9a052c1f934c5168387acde4dbb0378c9c7861cf
elixir88 <ognjen.andric@gmail.com>
2018-11-27 17:16:21 +0100

    Update Individual.java

--- Individual.java
+++ Individual.java
@@ -1,74 +1,68 @@
 public class Individual {
   //Attributes
-  private City[] cities;
+  private City[] individualPath;
 
   //Constructor
-  public Individual(City[] cities) {
+  Individual(City[] cities) {
     //copies the array to avoid issues with pointers
-    individual = new City[cities.length];
+    individualPath = new City[cities.length];
     for(int i=0; i<cities.length; i++)
-      individual[i] = cities[i]; //.copy(); //er ikke sikker på om det er nødvendigt at lave copy her.
+      individualPath[i] = cities[i];
     //shuffles the array
-    for(int i=0; i<individual.length; i++) {
-      int randomIndex = RandomUtils.getRandomValue(individual.length);
-      City auxiliaryVariable = cities[randomIndex];
-      individual[randomIndex] = individual[i];
-      individual[i] = auxiliaryVariable;
+    for(int i=0; i<individualPath.length; i++) {
+      int randomIndex = RandomUtils.getRandomValue(individualPath.length);
+      while(randomIndex == i)
+        randomIndex = RandomUtils.getRandomValue(individualPath.length);
+      City auxiliaryVariable = individualPath[randomIndex];
+      individualPath[randomIndex] = individualPath[i];
+      individualPath[i] = auxiliaryVariable;
     }
   }
 
-  //Returns a copy of this individual's path.
   public City[] path() {
-   City[] copy = new City[individual.length];
-   for(int i=0; i<individual.length; i++)
-      copy[i] = individual[i];
-   return copy;
-  }
-
-  //Returns the cost assigned to this individual's path.
-  public double cost() {
-    double sumOfCost = 0;
-    double costBetweenTwoCities = 0;
-    for(int i=1; i<individual.length; i++) {
-      costBetweenTwoCities = individual[i-1].distanceTo(individual[i]);
-      sumOfCost = sumOfCost + costBetweenTwoCities;
-
-    }
-    return sumOfCost + individual[individual.length-1].distanceTo(individual[0]);
-  }
-
-  //Performs a mutation of this individual.
-  public void mutate() {
-    int randomIndex = RandomUtils.getRandomValue(individual.length);
-    int lastIndex = individual.length - 1;
-    City auxiliaryVariable = individual[randomIndex]; //Pointer issue??
-    individual[randomIndex] = individual[lastIndex];
-    individual[lastIndex] = auxiliaryVariable;
-  }
- 
-  //Returns a new Individual differing from this one by a mutiation.
-  public Individual reproduce() {
-    City[] offspring = new City[individual.length];
-    for(int i = 0; i < offspring.length; i++){
-      offspring[i] = individual[i];
-    }
-    int randomIndex = RandomUtils.getRandomValue(offspring.length);
-    City auxiliaryVariable = offspring[randomIndex];
-    individual[randomIndex] = offspring[offspring.length-1];
-    offspring[offspring.length-1] = auxiliaryVariable;
-    return 
+    City[] copy = new City[individualPath.length];
+    for(int i=0; i<individualPath.length; i++)
+      copy[i] = individualPath[i];
+    return copy;
   }
 
   public String toString() {
     String city = "";
     String path = "";
-    for(City c : individual) {
+    for(City c : individualPath) {
       city = c.name() + ", ";
       path = path + city;
     }
     return path;
   }
 
+  public double cost() {
+    double sumOfCost = 0;
+    double costBetweenTwoCities = 0;
+    for(int i=1; i<individualPath.length; i++) {
+      costBetweenTwoCities = individualPath[i-1].distanceTo(individualPath[i]);
+      sumOfCost = sumOfCost + costBetweenTwoCities;
+
+    }
+    return sumOfCost + individualPath[individualPath.length-1].distanceTo(individualPath[0]);
+  }
+
+
+  public void mutate() {
+    int randomIndexOne = RandomUtils.getRandomValue(individualPath.length);
+    int randomIndexTwo = RandomUtils.getRandomValue(individualPath.length);
+    City auxiliaryVariable = individualPath[randomIndexOne];
+    individualPath[randomIndexOne] = individualPath[randomIndexTwo];
+    individualPath[randomIndexTwo] = auxiliaryVariable;
+  }
+
+
+  //Returns a new Individual differing from this one by a mutiation.
+  public Individual reproduce() {
+    Individual offspring = new Individual(path());
+    offspring.individualPath = path();
+    offspring.mutate();
+    return offspring;
+  }
+
 }
-
-
